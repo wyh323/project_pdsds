683092ee33abfbc03801d86bdaf98bdebb0733d2
wyh323 <2072887718@qq.com>
2025-03-04 20:42:40 +0800

    注册登录接口

--- back_end/pdsds/src/main/java/com/happy_hao/pdsds/dto/PatientLogin.java
+++ back_end/pdsds/src/main/java/com/happy_hao/pdsds/dto/PatientLogin.java
@@ -6,12 +6,12 @@ import lombok.Data;
 
 @Data
 public class PatientLogin {
-    @NotBlank(message = "用户名不能为空")
-    @Pattern(regexp = "^[a-zA-Z0-9_]{5,20}$", message = "用户名必须是5-20位的字母、数字或下划线")
+    @NotBlank(message = "The username cannot be empty.")
+    @Pattern(regexp = "^[a-zA-Z0-9_]{5,20}$", message = "The username must consist of 5 to 20 letters, digits or underscores.")
     private String username;
 
-    @NotBlank(message = "密码不能为空")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{8,20}$", message = "密码必须是8-20位，包含字母和数字")
+    @NotBlank(message = "The password cannot be empty.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{8,20}$", message = "The password must be 8 to 20 characters long and contain both letters and numbers.")
     private String password;
 
 }
